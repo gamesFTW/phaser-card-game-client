2aea652726843469ddd92490345c2663f0ec264b
itsacat <allah.taskbar@gmail.com>
2016-08-01 22:35:12 +0300

    fixed #44

--- src/cardGroups/hand/HandView.js
+++ src/cardGroups/hand/HandView.js
@@ -2,4 +2,13 @@ import CardRowViewManager from './../cardViewManagers/CardRowViewManager';
 
 
 export default class HandView extends CardRowViewManager {
+
+
+    /**
+     * @param {Card[]} cards
+     */
+    reorderCards(cards) {
+        super.reorderCards(cards);
+        cards.forEach(card => card.cardView.visible = true);
+    }
 }
